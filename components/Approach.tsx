f32e28e34148b6312f6b2c6963189ed3b6f6c4b7
Ramakm <itsramakrushna@gmail.com>
2024-06-02 15:09:30 +0000

    commit changes

--- components/Approach.tsx
+++ components/Approach.tsx
@@ -13,9 +13,9 @@ const Approach = () => {
       <div className="my-20 flex flex-col lg:flex-row items-center justify-center w-full gap-4">
         {/* add des prop */}
         <Card
-          title="Planning & Strategy"
+          title="Idea, Planning & Strategy"
           icon={<AceternityIcon order="Phase 1" />}
-          des="We'll collaborate to map out your website's goals, target audience, 
+          des="We'll listen to map your product goals, target audience, 
           and key functionalities. We'll discuss things like site structure, 
           navigation, and content requirements."
         >
@@ -28,8 +28,8 @@ const Approach = () => {
         <Card
           title="Development & Progress Update"
           icon={<AceternityIcon order="Phase 2" />}
-          des="Once we agree on the plan, I cue my lofi playlist and dive into
-          coding. From initial sketches to polished code, I keep you updated
+          des="Once we agree on the plan, we will try to help as much as we can and help you dive into
+          coding Phase. From initial sketches to polished code, We & your team will be in touch at
           every step of the way."
         >
           <CanvasRevealEffect
@@ -48,11 +48,11 @@ const Approach = () => {
           {/* <div className="absolute inset-0 [mask-image:radial-gradient(400px_at_center,white,transparent)] bg-black/50 dark:bg-black/90" /> */}
         </Card>
         <Card
-          title="Development & Launch"
+          title="Review & Launch"
           icon={<AceternityIcon order="Phase 3" />}
           des="This is where the magic happens! Based on the approved design, 
-          I'll translate everything into functional code, building your website
-          from the ground up."
+          Our product team will review everything,from building your product
+          to the ground up, From revenie to sharing percentage."
         >
           <CanvasRevealEffect
             animationSpeed={3}

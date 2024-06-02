fb3800d0009026120300c5e22bca4469af790278
Ramakm <itsramakrushna@gmail.com>
2024-06-02 15:03:04 +0000

    Commit changes

--- data/index.ts
+++ data/index.ts
@@ -214,13 +214,17 @@ export const socialMedia = [
   {
     id: 1,
     img: "/git.svg",
+    link: "https://github.com/your-profile",
   },
   {
     id: 2,
     img: "/twit.svg",
+    link: "https://twitter.com/growtechie",
   },
   {
     id: 3,
     img: "/link.svg",
+    link: "https://www.linkedin.com/company/growtechieind",
   },
 ];
+
